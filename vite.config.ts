2f318edda5dfe676e646b99967056fddc8f4fffa
unknown <instamy727@gmail.com>
2026-03-27 23:28:32 +0530

    setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/QRMENU/',
+  base: '/QRMENU/',
   server: {
     host: true,
     port: 5174,
